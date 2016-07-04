62facac8df3006f82cf3659c8cc8f3cf3dab7402
Esko Cruzeta <eskocruz@users.noreply.github.com>
2016-07-04 08:51:16 -0700

    init commit

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+	var checkPageButton = document.getElementById('checkPage');
+
+	checkPageButton.addEventListener('click', function() {
+
+		chrome.tabs.getSelected(null, function(tab) {
+			
+		});
+
+	})
+})
